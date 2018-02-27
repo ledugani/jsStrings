0fd427b375af8ba27df5ec25b48a55fa0c14e01e
Tom Dugan <thomas@aadwebb.com>
2018-02-26 20:30:40 -0600

    completed exercise 2

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 var numOfFriends = 4;
 var numOfSandwiches = numOfFriends * 2;
 console.log("numOfSandwiches", numOfSandwiches);
+
+// Challenge #2
+var name = "Tom";
+var string = "Hello, " + name + " how are you doing today?";
+console.log("string:", string);
+
