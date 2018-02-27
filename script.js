1a31f035fff1c46a8fa0c9ff87470a52d3d58c19
Tom Dugan <thomas@aadwebb.com>
2018-02-26 20:41:39 -0600

    completed exercise 3

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ var name = "Tom";
 var string = "Hello, " + name + " how are you doing today?";
 console.log("string:", string);
 
+// Challenge #3
+var DNA = "GCAT";
+var RNA = DNA.replace(/T/g, "U");
+console.log("RNA:", RNA);
